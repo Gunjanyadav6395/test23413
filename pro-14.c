9de421103177e3e04344f2e447ae10e539e855a9
Gunjanyadav6395 <111346545+Gunjanyadav6395@users.noreply.github.com>
2022-08-22 12:14:17 +0530

    Add files via upload

---
+++ pro-14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j==1||i==j||i==n)
+                printf("*");
+            else
+                printf(" ");
+        }
+            printf("\n");
+    }
+}
